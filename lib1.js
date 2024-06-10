f4b03f92826470d6394ef9f11011505bc6467a16
Sanil Singh <129430934+singhster7@users.noreply.github.com>
2024-06-11 00:21:41 +0530

    circular dependency

---
+++ lib1.js
@@ -0,0 +1,26 @@
+let lib2 = require('./lib2');
+
+console.log("Running lib1");
+let a = 10;
+
+module.exports = {
+    a,
+    lib2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+const lib1 = require('./lib1');
+const lib2 = require('./lib2');
+
+console.log(lib1.lib2.b);
+console.log(lib2.lib1.a);
+//dependency map explained in notebook
