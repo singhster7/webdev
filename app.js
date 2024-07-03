f7739dafff0b99db178a55e8f5a96b3d04ed5a6b
Sanil Singh <129430934+singhster7@users.noreply.github.com>
2024-07-03 14:07:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,72 @@
-const express = require('express')
-const app = express()
 const path = require('path');
-const PORT = 3000;
-// let publicPath = path.join(__dirname,"public");
-//app.use(express.static(publicPath))
-app.use(express.static(path.join(__dirname,"public")));
+const express = require('express');
+const app = express();
+const PORT = 4444;
+const hbs = require('hbs');
 
+hbs.registerPartials(path.join(__dirname + '/views/partials'));//partials ke liye
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-    res.send("hello sir")
-})
-app.get('/greetings', (req,res) => {
-    res.send("hello bsss sir")
-})
-//1..params wala tarika
-app.get('/greet/:name', (req,res) => {//yaha name key hai
-    console.log(req.params);
-   // console.log(`KYA BE ${req.params.name}`) terminal me orint hoga jo bhi url me likhega /greet/ ke baad
-    res.send(` kya be ${req.params.name}`)
-    
-})
-//2..query parameter
-// Query parameters req: '/bye?name=aman&city=delhi
-app.get('/bye',(req,res)=>{
-    res.send(`Aur, Chalo Milte hai baad mei? ${req.query.name} from ${req.query.city}`);
+let todos = ["Cricket", "Dance", "Sing", "Hockey"];
+
+app.get('/', (req, res) => {
+    res.render('index', {//res.render tmapltes me use hoga
+        //index jisme file hai temlate wali,views ke andar
+        firstname: "Rahul",
+        lastname: "Yadav",
+        todos
+    })
 })
 
-//ye jo line hai niche wo nhi chalegi aagar path require/import nhi hai to
-app.get('/homee',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+app.post('/todos', async (req, res) => {
+    try {
+        const { newTask } = req.body;
+        todos.push(newTask);
+        res.redirect('/');
+    }
+    catch (err) {
+        res.send(err);
+    }
 })
 
-//We don't want to do this manually one by one
-// app.get('/script.js',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'script.js'));
-// })
+app.get('/deletetask', async (req, res) => {
+    try {
+        const { name } = req.query;
+        todos = todos.filter(t => t!==name);
+        res.redirect('/');
+    }
+    catch (err) {
+        res.send(err);
+    }
+})
 
-// app.get('/style.css',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'style.css'));
-// })
+app.get('/increase', (req, res) => {
+    try {
+        const { name } = req.query;
+        let indx = todos.indexOf(name);
+        if (indx > 0)
+            [todos[indx - 1], todos[indx]] = [todos[indx], todos[indx - 1]];
+        res.redirect('/');
+    }
+    catch (err) {
+        res.send(err);
+    }
+})
 
-// Alternative to the above part is to send all the files inside a folder to the
-// browser at once
+app.get('/decrease', (req, res) => {
+    try {
+        const { name } = req.query;
+        let indx = todos.indexOf(name);
+        if (indx < todos.length-1)
+            [todos[indx + 1], todos[indx]] = [todos[indx], todos[indx + 1]];
+        res.redirect('/');
+    }
+    catch (err) {
+        res.send(err);
+    }
+})
 
-
-
-//slash greetings wale ispe response alag hai 
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`);
-}); // This will start the server
+app.listen(PORT, () => {
+    console.log(`http://localhost:` + PORT);
+});
